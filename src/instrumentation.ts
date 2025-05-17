ed391e2a8d580dd73f3055bcc0941ff11855d13c
yoichi <sunnyon41@gmail.com>
2025-05-17 15:42:26 +0900

    Add debug code

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,10 +1,23 @@
 import { registerOTel } from "@vercel/otel";
 import { getLangfuseExporter } from "./mastra/langfuse-exporter";
 
+console.error("Import meta outer");
+console.error(import.meta.url);
+
 export function register() {
   console.error("Debugging register");
   console.error(registerOTel as any);
   console.error(registerOTel.toString());
+
+
+  console.error("Import meta inner");
+  console.error(import.meta.url);
+  const resolve = import.meta.resolve as any;
+  if (resolve) {
+    console.error(resolve("@vercel/otel"));
+  } else {
+    console.error("resolve is not found");
+  }
 
   registerOTel({
     serviceName: "ai",
