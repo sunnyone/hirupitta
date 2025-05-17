76bb60a91d1c9c228d3a7a54afb0736bab4a8922
yoichi <sunnyon41@gmail.com>
2025-05-17 15:30:38 +0900

    Add debugging code

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,6 +2,10 @@ import { registerOTel } from "@vercel/otel";
 import { getLangfuseExporter } from "./mastra/langfuse-exporter";
 
 export function register() {
+  console.error("Debugging register");
+  console.error(registerOTel as any);
+  console.error(registerOTel.toString());
+
   registerOTel({
     serviceName: "ai",
     traceExporter: getLangfuseExporter(),
