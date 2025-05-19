f51bc725dc81a0d7d023a0a05e8fe5afaac1acaf
yoichi <sunnyon41@gmail.com>
2025-05-20 02:14:52 +0900

    Move to root

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,30 @@
-console.log("Root instrumentation.ts");
+import { registerOTel } from "@vercel/otel";
+import { getLangfuseExporter } from "./src/mastra/langfuse-exporter";
+import { waitUntil } from "@vercel/functions";
 
-export async function register() {
-    console.log("Root register called");
+console.error("Import meta outer");
+console.error(import.meta.url);
+
+export function register() {
+  console.error("Debugging register");
+  console.error(registerOTel as any);
+  console.error(registerOTel.toString());
+
+  console.error("Import meta inner");
+  console.error(import.meta.url);
+  const resolve = import.meta.resolve as any;
+  if (resolve) {
+    console.error(resolve("@vercel/otel"));
+  } else {
+    console.error("resolve is not found");
+  }
+
+  const exporter = getLangfuseExporter();
+  registerOTel({
+    serviceName: "ai",
+    traceExporter: exporter,
+  });
+  waitUntil(exporter.forceFlush());
 }
+
+throw new Error("instrumentation.ts is called")
