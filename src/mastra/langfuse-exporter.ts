d29521a1b49aacabb0a0002a56d67b09bc92a1cc
yoichi <sunnyon41@gmail.com>
2025-05-20 00:00:22 +0900

    add log

--- src/mastra/langfuse-exporter.ts
+++ src/mastra/langfuse-exporter.ts
@@ -3,13 +3,17 @@ import { LangfuseExporter } from "langfuse-vercel";
 let exporter: LangfuseExporter | undefined;
 
 export function getLangfuseExporter() {
+    console.log("Get Langfuse exporter");
     if (!exporter) {
+        console.log("Creating new");
         exporter = new LangfuseExporter({
             publicKey: process.env.LANGFUSE_PUBLIC_KEY,
             secretKey: process.env.LANGFUSE_SECRET_KEY,
             baseUrl: process.env.LANGFUSE_HOST,
         });
+    } else {
+        console.log("Existing one.")
     }
-    
+
     return exporter;
 }
